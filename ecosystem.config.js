107166041b01b44add4625bcbd57e3121ab7f37c
wangweiguo <wangweiguo_v@didiglobal.com>
2021-10-09 17:32:19 +0800

    feat:生产环境改用编译后代码

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,8 +5,8 @@ module.exports = {
     apps: [
         {
             name: 'API',
-            script: 'app.ts',
-            interpreter: './node_modules/.bin/ts-node',
+            script: 'app.js',
+            // interpreter: './node_modules/.bin/ts-node',
             // 解释器参数 -P 表示项目路径，会自动使用项目的 tsconfig.json
             // interpreter_args: '-r tsconfig-paths/register',
             instances: 1,
@@ -33,8 +33,6 @@ module.exports = {
                 REMOTE_ADDR: '',
                 watch: false,
             },
-            error_file: '/www/err.log', // 错误日志文件，必须设置在项目外的目录，这里为了测试
-            out_file: '/www/info.log', //  流水日志，包括 console.log 日志，必须设置在项目外的目录，这里为了测试
             max_restarts: 10,
         },
     ],
